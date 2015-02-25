46b4399222de363e9049cd936b682d8803e63133
abashev <abashev@playform.com>
2015-02-25 19:18:28 +0300

    added change

--- tpl/vapp.php
+++ tpl/vapp.php
@@ -61,7 +61,7 @@
 </script>
 
 <script type="text/template" id="vapp-navigation-item">
-    <a class="vapp-header_link unselectable fl-r {class}" id="vapp-header_link_{tabSource}" href="/?tab={tabSource}" onclick="vapp.feed.change('{tabSource}', event);">{title}</a>
+    <a class="vapp-header_link unselectable fl-r {class}" id="vapp-header_link_{tabSource}" href="/?tab={tabSource}" onclick="vapp.feed.friendId = null; vapp.feed.change('{tabSource}', event);">{title}</a>
 </script>
 
 <script type="text/javascript">
